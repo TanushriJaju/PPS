2de7530db4c65e2d27d36918982609bae4d567f7
TanushriJaju <tanujaju03@gmail.com>
2025-04-10 15:58:06 +0530

    linear search

--- linearsearch.c
+++ linearsearch.c
@@ -12,19 +12,26 @@ return -1;
 }
 int main()
 {
-int arr[]={10,20,30,40,50};
-int size = sizeof(arr);
-int key;
-printf("Enter the element to search:");
-scanf("%d", &key);
-int result = LinearSearch(arr,size,key);
-if(result != -1)
-{
-printf("Element found at index %d\n",result);
-}
-else
-{
-printf("Element not found in the array\n");
+ int n;
+ printf("Enter the number of elements of the array:");
+ scanf("%d", &n);
+
+ int arr[n];
+ printf("Enter the elements in the array:");
+ for(int i=0;i<n;i++)
+ scanf("%d", &arr[i]);
+
+ int key;
+ printf("Enter the element to search:");
+ scanf ("%d", &key);
+
+ int result=LinearSearch(arr,n,key);
+
+ if(result != -1){
+        printf("Element found at index %d\n", result);
+ }else{
+        printf("Element not found in the array\n");
 }
 return 0;
 }
+
